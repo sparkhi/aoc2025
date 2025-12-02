4f0ffbbe5bac49b149dd3bd42d1193aee7e0fb56
sparkhi <4743002+sparkhi@users.noreply.github.com>
2025-12-02 20:35:39 +0000

    Day 1 part 2 - done

--- src/main/scala/com/knowingwhere/aoc2025/Day1.scala
+++ src/main/scala/com/knowingwhere/aoc2025/Day1.scala
@@ -5,11 +5,14 @@ import scala.io.Source
 import scala.util.chaining.scalaUtilChainingOps
 
 object Day1 extends App :
-  val lines = Source.fromResource("day1-input.txt").getLines().toList
+  private val lines = Source.fromResource("day1-input.txt").getLines().toList
   val start = 50
 
-  val counter = getCountForDialReachingZero(lines, 50, 0)
+  private val counter = getCountForDialReachingZero(lines, 50, 0)
   counter.pipe(println)
+
+  private val counter2 = getCountForDialCrossingZero(lines, 50, 0)
+  counter2.pipe(println)
 
   @tailrec
   def getCountForDialReachingZero(movements: List[String], initialVal: Int, countOfZero: Int): Int = {
@@ -18,53 +21,54 @@ object Day1 extends App :
         countOfZero
       case _ =>
         val instruction = movements.head
-        val direction = instruction.substring(0,1)
         val clicks = instruction.substring(1).toInt
-        val newPos = (direction match {
-          case "L" =>
-            initialVal - clicks
-          case "R" =>
-            initialVal + clicks
-        }) % 100
-        if (newPos < 0) {
-          getCountForDialReachingZero(movements.tail, 100 + newPos, countOfZero)
-        } else if (newPos > 0) {
-          getCountForDialReachingZero(movements.tail, newPos, countOfZero)
-        } else {
-          getCountForDialReachingZero(movements.tail, 0, countOfZero + 1)
+        val newPos = instruction.head match {
+          case 'L' => initialVal - clicks
+          case 'R' => initialVal + clicks
         }
-    }
 
-    @tailrec
-    def getCountForDialCrossingZero(movements: List[String], initialVal: Int, countOfCrossingZero: Int): Int = {
-      movements match {
-        case Nil =>
-          countOfCrossingZero
-        case _ =>
-          val instruction = movements.head
-          val direction = instruction.substring(0, 1)
-          val clicks = instruction.substring(1).toInt
-          val newPos = direction match {
-            case "L" =>
-              initialVal - clicks
-            case "R" =>
-              initialVal + clicks
-          }
-          
-          val numberOfFullTurns = newPos / 100
-          val resultantPos = newPos % 100
-          
-          // Dial will cross zero number of times as below:
-          // If positive to negative => 1 + numberOfFullTurns
-          // If negative to positive => 1 + numberOfFullTurns
-          // If 0 to anywhere => numberOfFullTurns
-          
-          if (newPos < 0) {
-            getCountForDialReachingZero(movements.tail, 100 + newPos, countOfZero)
-          } else if (newPos > 0) {
-            getCountForDialReachingZero(movements.tail, newPos, countOfZero)
-          } else {
-            getCountForDialReachingZero(movements.tail, 0, countOfZero + 1)
-          }
-      }
+        val resultantPos = newPos % 100
+        val (nextPos, nextZeroCount) = resultantPos match {
+          case n if n < 0 => (100 + resultantPos, countOfZero)
+          case n if n > 0 => (n, countOfZero)
+          case _ => (0, countOfZero + 1)
+        }
+        getCountForDialReachingZero(movements.tail, nextPos, nextZeroCount)
     }
+  }
+
+  @tailrec
+  def getCountForDialCrossingZero(movements: List[String], initialVal: Int, countOfCrossingZero: Int): Int = {
+    movements match {
+      case Nil =>
+        countOfCrossingZero
+      case _ =>
+        val instruction = movements.head
+        val clicks = instruction.substring(1).toInt
+        val zeroCrossingCount = getZeroCrossingCount(initialVal, clicks, instruction.head)
+
+        val newPos = (instruction.head match {
+          case 'L' => initialVal - clicks
+          case 'R' => initialVal + clicks
+        }) % 100
+
+        val normalisedNewPos = if newPos < 0 then 100 + newPos else newPos
+
+        getCountForDialCrossingZero(movements.tail, normalisedNewPos, countOfCrossingZero + zeroCrossingCount)
+    }
+  }
+
+  def getZeroCrossingCount(initial: Int, clicks: Int, direction: Char): Int = {
+    val turns = clicks / 100
+    val resultant = clicks % 100
+    val incompleteCrossing = direction match {
+      case 'L' =>
+        if initial == 0 then 0
+        else if resultant >= initial then 1 else 0
+      case 'R' =>
+        if initial == 0 then 0
+        else
+          if resultant >= (100 - initial) then 1 else 0
+    }
+    incompleteCrossing + turns
+  }
--- src/test/scala/com/knowingwhere/aoc2025/Day1Test.scala
+++ src/test/scala/com/knowingwhere/aoc2025/Day1Test.scala
@@ -32,7 +32,38 @@ class Day1Test extends AnyWordSpec with BeforeAndAfterEach {
       count shouldBe 3
     }
     "generate the number of times the dial crosses zero as 6" in {
-      
+      val count = Day1.getCountForDialCrossingZero(movements, 50, 0)
+      count shouldBe 6
+    }
+    "get zero crossing count as 0 for simple addition and subtraction cases " in {
+      Day1.getZeroCrossingCount(10, 20, 'R') shouldBe 0
+      Day1.getZeroCrossingCount(10, 89, 'R') shouldBe 0
+      Day1.getZeroCrossingCount(0, 20, 'R') shouldBe 0
+      Day1.getZeroCrossingCount(70, 40, 'L') shouldBe 0
+      Day1.getZeroCrossingCount(50, 48, 'L') shouldBe 0
+      Day1.getZeroCrossingCount(0, 20, 'L') shouldBe 0
+    }
+    "get zero crossing count as 1 for simple addition and subtraction cases " in {
+      Day1.getZeroCrossingCount(90, 20, 'R') shouldBe 1
+      Day1.getZeroCrossingCount(60, 40, 'R') shouldBe 1
+      Day1.getZeroCrossingCount(20, 30, 'L') shouldBe 1
+      Day1.getZeroCrossingCount(15, 15, 'L') shouldBe 1
+    }
+    "get zero crossing count as 2 when the dial turns more than one full circle" in {
+      Day1.getZeroCrossingCount(90, 120, 'R') shouldBe 2
+      Day1.getZeroCrossingCount(60, 140, 'R') shouldBe 2
+      Day1.getZeroCrossingCount(20, 130, 'L') shouldBe 2
+      Day1.getZeroCrossingCount(15, 115, 'L') shouldBe 2
+    }
+    "get zero crossing count when the dial turns lots of circles than one full circle" in {
+      Day1.getZeroCrossingCount(90, 111, 'R') shouldBe 2
+      Day1.getZeroCrossingCount(60, 440, 'R') shouldBe 5
+      Day1.getZeroCrossingCount(20, 330, 'L') shouldBe 4
+      Day1.getZeroCrossingCount(15, 120, 'L') shouldBe 2
+    }
+    "get zero crossing count when starting at zero and going exact full circle" in {
+      Day1.getZeroCrossingCount(0, 100, 'L') shouldBe 1
+      Day1.getZeroCrossingCount(0, 100, 'R') shouldBe 1
     }
   }
 }
